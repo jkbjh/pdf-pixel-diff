0ca90d7b133626f55366c1a0c77167eb8a861541
Jakob Hollenstein <517680+jkbjh@users.noreply.github.com>
2023-08-16 14:17:35 +0200

    add checking of files, add temporary files

--- pdf_pixel_diff/__main__.py
+++ pdf_pixel_diff/__main__.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 import argparse
+import os
 import subprocess
+import tempfile
 
 import numpy as np
 from PIL import Image
 
 
 def convert_pdf_to_png(pdf_file, png_file):
-    subprocess.check_call(["convert", "-density", "75", pdf_file, "-define", "png:color-type=6", png_file])
+    subprocess.check_call(["convert", "-density", "75", "-define", "png:color-type=6", pdf_file, png_file])
 
 
 def compare_images(image1, image2):
@@ -34,16 +36,19 @@ def main():
     parser.add_argument("pdf2", help="Path to the second PDF file")
     args = parser.parse_args()
 
-    png1 = "pdf1.png"
-    png2 = "pdf2.png"
+    if not os.path.exists(args.pdf1):
+        raise FileNotFoundError(f"Input PDF file '{args.pdf1}' not found.")
+    if not os.path.exists(args.pdf2):
+        raise FileNotFoundError(f"Input PDF file '{args.pdf2}' not found.")
 
-    convert_pdf_to_png(args.pdf1, png1)
-    convert_pdf_to_png(args.pdf2, png2)
+    with tempfile.NamedTemporaryFile(suffix=".png") as tmp_png1, tempfile.NamedTemporaryFile(suffix=".png") as tmp_png2:
+        convert_pdf_to_png(args.pdf1, tmp_png1.name)
+        convert_pdf_to_png(args.pdf2, tmp_png2.name)
 
-    if compare_images(png1, png2):
-        exit(0)  # Signal to Git that files are the same
-    else:
-        exit(1)  # Signal to Git that files are different
+        if compare_images(tmp_png1.name, tmp_png2.name):
+            exit(0)  # Signal to Git that files are the same
+        else:
+            exit(1)  # Signal to Git that files are different
 
 
 if __name__ == "__main__":
