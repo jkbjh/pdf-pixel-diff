c485193e470ed125611dd2817f1e86980da769ad
Jakob Hollenstein <517680+jkbjh@users.noreply.github.com>
2023-08-16 14:35:06 +0200

    with ascii art output

--- pdf_pixel_diff/__main__.py
+++ pdf_pixel_diff/__main__.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 import tempfile
 
+import ascii_magic
 import numpy as np
 from PIL import Image
 
@@ -12,30 +13,15 @@ def convert_pdf_to_png(pdf_file, png_file):
     subprocess.check_call(["convert", "-density", "75", "-define", "png:color-type=6", pdf_file, png_file])
 
 
-def create_diff_image(image1, image2, output):
-    subprocess.run(["compare", "-metric", "AE", "-fuzz", "5%", image1, image2, output])
-
-
-def compare_images(image1, image2):
-    img1 = np.array(Image.open(image1))
-    img2 = np.array(Image.open(image2))
-
-    if img1.shape != img2.shape:
-        return False
-
-    pixel_diff = np.sum(img1 != img2)
-
-    if pixel_diff == 0:
-        return True
-    else:
-        create_diff_image(image1, image2, "diff.png")
-        return False
+def create_pixel_diff_image(image1, image2, diff_image):
+    subprocess.run(["compare", "-metric", "AE", "-fuzz", "5%", image1, image2, diff_image])
 
 
 def main():
     parser = argparse.ArgumentParser(description="Custom PDF diff tool for Git")
     parser.add_argument("pdf1", help="Path to the first PDF file")
     parser.add_argument("pdf2", help="Path to the second PDF file")
+    parser.add_argument("--asciiart", action="store_true", help="Convert diff image to ASCII art and print to console")
     args = parser.parse_args()
 
     if not os.path.exists(args.pdf1):
@@ -47,9 +33,20 @@ def main():
         convert_pdf_to_png(args.pdf1, tmp_png1.name)
         convert_pdf_to_png(args.pdf2, tmp_png2.name)
 
-        if compare_images(tmp_png1.name, tmp_png2.name):
+        pixel_diff = np.sum(np.array(Image.open(tmp_png1.name)) != np.array(Image.open(tmp_png2.name)))
+
+        if pixel_diff == 0:
             exit(0)  # Signal to Git that files are the same
         else:
+            diff_image = "diff.png"
+            create_pixel_diff_image(tmp_png1.name, tmp_png2.name, diff_image)
+
+            if args.asciiart:
+                ascii_art = ascii_magic.from_image(diff_image)
+                ascii_art.to_terminal()
+            else:
+                print("Files are different, use --asciiart flag to view ASCII art representation.")
+
             exit(1)  # Signal to Git that files are different
 
 
