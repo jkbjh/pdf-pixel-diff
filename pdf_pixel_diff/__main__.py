29ab4d6528cbe1ebc5b230f15d2ef12207653baf
Jakob Hollenstein <517680+jkbjh@users.noreply.github.com>
2023-08-16 15:43:08 +0200

    with nice ascii art

--- pdf_pixel_diff/__main__.py
+++ pdf_pixel_diff/__main__.py
@@ -4,9 +4,29 @@ import os
 import subprocess
 import tempfile
 
-import ascii_magic
+import img2unicode
 import numpy as np
 from PIL import Image
+
+
+def scale_image_to_fit(image_path, output_path):
+    cols, rows = os.get_terminal_size()
+    pixel_multiplier = 8
+    width, height = cols * pixel_multiplier, rows * pixel_multiplier
+    img = Image.open(image_path)
+    ratio = np.minimum(width / img.width, height / img.height)
+    new_width, new_height = (int(img.width * ratio), int(img.height * ratio))
+    resized_img = img.resize((new_width, new_height))
+    resized_img.save(output_path)
+
+
+def to_ascii_art(in_png, out_txt):
+    # Use Unicode Block Elements
+    cols, rows = os.get_terminal_size()
+    scale_image_to_fit(in_png, "diff.scaled.png")
+    optimizer = img2unicode.FastQuadDualOptimizer()
+    renderer = img2unicode.Renderer(default_optimizer=optimizer, max_h=rows, max_w=cols)
+    renderer.render_terminal("diff.scaled.png", out_txt)
 
 
 def convert_pdf_to_png(pdf_file, png_file):
@@ -14,8 +34,20 @@ def convert_pdf_to_png(pdf_file, png_file):
 
 
 def create_pixel_diff_image(image1, image2, diff_image):
-    subprocess.run(["compare", "-metric", "AE", "-fuzz", "5%", image1, image2, diff_image])
-    subprocess.run(["convert", diff_image, "-blur", "0x3", diff_image])  # Apply a blur filter
+    # subprocess.run(["compare", "-metric", "AE", "-fuzz", "5%", image1, image2, diff_image])
+    # subprocess.run(["compare", "-metric", "AE", "-fuzz", "5%", image1, image2, diff_image])
+    # subprocess.run(["compare", "-density", "300", image1, image2, "-compose", "src", diff_image])# diff.jpeg
+    subprocess.run(
+        "convert '(' " + image1 + " -flatten -grayscale Rec709Luminance ')' "
+        "'(' " + image2 + " -flatten -grayscale Rec709Luminance ')' "
+        "'(' -clone 0-1 -compose darken -composite ')' "
+        "-channel RGB -combine " + diff_image,
+        shell=True,
+    )
+
+
+def apply_image_magick_operations(image_path):
+    subprocess.check_call(["convert", image_path, "-blur", "0x3", "-contrast-stretch", "10%", image_path])
 
 
 def main():
@@ -30,7 +62,10 @@ def main():
     if not os.path.exists(args.pdf2):
         raise FileNotFoundError(f"Input PDF file '{args.pdf2}' not found.")
 
-    with tempfile.NamedTemporaryFile(suffix=".png") as tmp_png1, tempfile.NamedTemporaryFile(suffix=".png") as tmp_png2:
+    with tempfile.NamedTemporaryFile(suffix=".png") as tmp_png1, tempfile.NamedTemporaryFile(
+        suffix=".png"
+    ) as tmp_png2, tempfile.NamedTemporaryFile(suffix=".png") as tmp_diff:
+        del tmp_diff
         convert_pdf_to_png(args.pdf1, tmp_png1.name)
         convert_pdf_to_png(args.pdf2, tmp_png2.name)
 
@@ -42,9 +77,13 @@ def main():
             diff_image = "diff.png"
             create_pixel_diff_image(tmp_png1.name, tmp_png2.name, diff_image)
 
+            # apply_image_magick_operations(diff_image)
+
             if args.asciiart:
-                ascii_art = ascii_magic.from_image(diff_image)
-                ascii_art.to_terminal()
+                to_ascii_art(diff_image, "diff.txt")
+                with open("diff.txt", "rt") as fobj:
+                    print()
+                    print(fobj.read())
             else:
                 print("Files are different, use --asciiart flag to view ASCII art representation.")
 
